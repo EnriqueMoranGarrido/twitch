4424bf3c8690a5282f700788184b2a6d4e0f6ea8
Enrique Moran <enrique.morango@udlap.mx>
2022-06-18 11:56:43 -0500

    auth with google and github

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import Image from "next/image";
 
 const Account = () => {
   const { data: session } = useSession();
@@ -8,8 +9,22 @@ const Account = () => {
     return (
       <div className="pt-[100px] flex flex-col max-w-xl w-full mx-auto p-4">
         <h3 className="text-3xl font-bold">Welcome, {session.user.name} </h3>
-        <p>Signed in as {session.user.email}</p>
-        <button onClick={() => signOut()}>Sign out</button>
+        <p className="py-4">Signed in as {session.user.email}</p>
+        <div className="pb-4 m-auto">
+          <Image
+            src={session.user.image}
+            alt=""
+            width={"150%"}
+            height={"150%"}
+            className="rounded-full"
+          />
+        </div>
+        <button
+          className="flex items-center justify-center p-3 bg-gray-700 border border-gray-700 my-2 "
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
       </div>
     );
   }
@@ -23,6 +38,14 @@ const Account = () => {
       >
         <FaGithub className="mr-2" size={25} />
         Sign in with <span className="font-bold pl-1">GitHub</span>
+      </button>
+
+      <button
+        onClick={() => signIn()}
+        className="flex items-center justify-center p-3 bg-blue-700 border border-gray-700 my-2"
+      >
+        <FaGoogle className="mr-2" size={25} />
+        Sign in with <span className="font-bold pl-1">Google</span>
       </button>
     </div>
   );
